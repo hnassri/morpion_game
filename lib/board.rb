8fe8a59de238794b8267ddf034a29987b06808e6
hnassri <nassrihamdi@gmail.com>
2019-01-24 14:24:45 +0100

    push

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,21 @@
 require 'bundler'
 Bundler.require
-
+require_relative 'board_case'
 class Board
 
 
-
-
+  def initialize
+    board_a1 = BoardCase.new("", "A1")
+    board_a2 = BoardCase.new("", "A1")
+    board_a3 = BoardCase.new("", "A1")
+    board_b1 = BoardCase.new("", "A1")
+    board_b2 = BoardCase.new("", "A1")
+    board_b3 = BoardCase.new("", "A1")
+    board_c1 = BoardCase.new("", "A1")
+    board_c2 = BoardCase.new("", "A1")
+    board_c3 = BoardCase.new("", "A1")
+   
+  end
 
 
 
--- lib/board_case.rb
+++ lib/board_case.rb
@@ -3,9 +3,11 @@ Bundler.require
 
 
 class BoardCase
-
-
-
+  attr_accessor :content, :position
+  def initialize(content, position)
+    @content = content
+    @position = position
+  end
 
 
 
